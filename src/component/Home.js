a69b717a9ed87b9da21f0eed76be8d44360a93ca
Repaa <75612768+Revalina29-Rpl@users.noreply.github.com>
2022-03-10 23:08:08 +0700

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+                    <p className='location-header'>Practicum 1/Home</p>
+                    <br></br><br></br><br></br><br></br><br/><br/><br/>
+                            <h5 className="card-title display-3 fw-bolder mb-0">VOILÀ,</h5>
+                            <p className="card-text lead fs-2 mb-0">THIS IS PRACTICUM 1</p>
+                            <p className="card-text lead mb-5">- BY REPAA -</p>
+                         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+               
+            </div>
+                
+    );
+};
+
+export default Home;
